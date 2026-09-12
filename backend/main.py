e3ce29306b20527660b88da97750352c84bd2e83
yusei <yusei20140625@gmail.com>
2026-09-13 01:46:20 +0900

    Update CORS for production frontend

--- backend/main.py
+++ backend/main.py
@@ -44,6 +44,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://heikinchi-web.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
